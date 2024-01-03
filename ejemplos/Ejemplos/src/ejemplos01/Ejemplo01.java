590c537ca260edf6253dfdf97f8b48f3f64bcf0b
CisneParra <adparra2@utpl.edu.ec>
2024-01-03 16:09:52 -0500

    cambios de la clase

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -16,25 +16,54 @@ public class Ejemplo01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-        String miCiudad = obtenerCiudadMayuscula("Loja");
-        System.out.printf("%s\n", miCiudad);
+
+        // String miCiudad = obtenerCiudadMayuscula("Loja");
+        // System.out.printf("%s\n", miCiudad);
+        obtenerMultiplicacion(7, 50); // no retorna nada ya que este
+                                // es un procedimiento no una funcion
+        String miMensaje = obtenerMultiplicacionDos(7, 50);
+        // por otro lado esta retorna informacion ya que es una funcion
+        System.out.printf("%s\n", miMensaje);
+
     }
-    
-    
-    public String obtenerCiudadMayuscula(String m){
+
+    public static String obtenerMultiplicacionDos(int tabla, int limite) {
+        int t = tabla;
+        int l = limite;
+        int m;
+        String ca = "";
+        for (int c = 0; c <= l; c++) {
+            m = t * c;
+            ca = String.format("%s%d * %d = %d\n", ca,
+                    t, c, m);
+            
+        }
+        return ca;
+    }
+
+    public static String obtenerCiudadMayuscula(String m) {
         String m2 = m.toUpperCase();
         return m2;
     }
-    
-    public static void obtenerMultiplicacion(int tabla, int limite){
-        
+
+    public static void obtenerMultiplicacion(int tabla, int limite) {
+        int t = tabla;
+        int l = limite;
+        int m;
+        String ca = "";
+        for (int c = 0; c <= l; c++) {
+            m = t * c;
+            ca = String.format("%s%d * %d = %d\n", ca,
+                    t, c, m);
+        }
+        System.out.printf("%s", ca);
+        {
+
+        }
     }
-    
-    public static String obtenerNombre(){
+
+    public static String obtenerNombre() {
         return "Luis";
     }
-    
-    
+
 }
