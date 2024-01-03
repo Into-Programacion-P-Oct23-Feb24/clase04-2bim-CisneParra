5d8b55d067efb203fe9673176c32bf1f112ccb1d
CisneParra <adparra2@utpl.edu.ec>
2024-01-03 16:54:24 -0500

    cambios de la clase

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -13,16 +13,32 @@ public class Ejemplo041 {
 
     public static void main(String[] args) {
         // 
-        obtenerMultiplicacion(primerValor, segundoValor); 
-        
+        int[][] primerValor = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
+        int[][] segundoValor = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
+        obtenerMultiplicacion(primerValor, segundoValor);
+
     }
+
+    public static void[][] obtenerMultiplicacion(int[][] a, int[][] b) {
+        int operacion;
+        int[][] m = new int[3][3];
+
+        for (int i = 0; i < a.length; i++) {
+            String acum = "";
+            for (int j = 0; j < a[i].length; i++) {
+                if (i == j) {
+                    operacion = a[i][j] * b[i][j];
+                    m[i][j] = operacion;
+                    acum = String.format("%s%d ", acum, m[i][j]);
+                }
+                acum = String.format("%s\n",acum);
+            }
+            System.out.printf("%s\n",acum);
+        }
         
-    public static void obtenerSuma(int a, int b){
-        int suma;
-        suma = a + b;
-        System.out.printf("El valor de la suma es: %d\n", suma);
-        
+
+        return m;
+
     }
-    
-    
+
 }
