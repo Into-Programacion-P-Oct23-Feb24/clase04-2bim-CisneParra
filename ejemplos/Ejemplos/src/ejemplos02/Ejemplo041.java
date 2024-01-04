6b81bec477a4d1cf8ab80e86974d2f28577a9927
Cisne Parra <adparra2@utpl.edu.ec>
2024-01-04 15:52:57 -0500

    cambios de la clase

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -11,43 +11,42 @@ package ejemplos02;
  */
 public class Ejemplo041 {
 
-    public static void main(String[] args) {
-        String cadenaFinal = "";
-        int[][] arreglo1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
-        int[][] arreglo2 = {{1, 2, 3}, {2, 2, 2}, {3, 2, 1}};
-        int[][] arreglo3 = new int[3][3];
-
+     public static void main(String[] args) {
+        // 
+        
+        int [][] arreglo1 = {{1,2,3}, {3,2,4}, {2,6,2}};
+        int [][] arreglo2 = {{1,2,3}, {2,2,2}, {3,1,2}};
+        int [][] arreglo3 = new int[3][3];
+        
         for (int i = 0; i < arreglo1.length; i++) {
             for (int j = 0; j < arreglo1[i].length; j++) {
-                arreglo3[i][j] = obtenerMultiplicacion(arreglo1[i][j],
+                arreglo3[i][j] = obtenerMultiplicacion(arreglo1[i][j], 
                         arreglo2[i][j]);
             }
         }
+        
+        obtenerReporte(arreglo1);
+        obtenerReporte(arreglo2);
+        obtenerReporte(arreglo3);
+    }
+    
+    public static void obtenerReporte(int[][] arreglo1){
+        String cadena = "";
         for (int i = 0; i < arreglo1.length; i++) {
             for (int j = 0; j < arreglo1[i].length; j++) {
-                arreglo3[i][j] = obtenerMultiplicacion(arreglo1[i][j], arreglo2[i][j]);
+                cadena = String.format("%s%d\t", cadena, arreglo1[i][j]);
             }
+            cadena = String.format("%s\n", cadena);
         }
-        for (int i = 0; i < arreglo1.length; i++) {
-            for (int j = 0; j < arreglo1[i].length; j++) {
-                System.out.printf(" %s %d", cadenaFinal, arreglo3[i][j]);
-                System.out.printf(" %d", arreglo1[i][j]);
-                System.out.printf(" %d", arreglo2[i][j]);
-                if (j == 2) {
-                    System.out.print("\n");
-                    if (i == 2) {
-                        System.out.print("\n");
-                    }
-                }
-            }
-        }
-
+        System.out.println(cadena);
     }
-
-    public static int obtenerMultiplicacion(int a, int b) {
-        int multiplicacion;
-        multiplicacion = a * b;
-        return multiplicacion;
+    
+    public static int obtenerMultiplicacion(int a, int b){
+        int operacion;
+        operacion = a * b;
+        return operacion;
+        
     }
-
+    
+    
 }
